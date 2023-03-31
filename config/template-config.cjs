9769d2615939a699d0796008cabd9674758ca004
Fenta <44316203+wkx160312746@users.noreply.github.com>
2023-03-31 14:32:49 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -51,6 +51,35 @@ const TEMPLATE_CONFIG = [
       成功用户: {{success_post_ids.DATA}}
       
       失败用户: {{fail_post_ids.DATA}}
+      
+      美好的一天从**我爱你**开始~!
+      
+下面是**闽侯**今天的天气~
+
+天气：**{{weather.DATA}}**
+
+气温: **{{min_temperature.DATA}} ~ {{max_temperature.DATA}}**
+
+男友提示: **{{notice.DATA}}哦~**
+
+---
+
+考完试累了吧~吃个外卖犒劳一下
+
+今日外卖:  **{{waimai.DATA}}**
+
+未拿快递: **{{no_express.DATA}}**
+
+---
+
+今天是我们恋爱的第**{{love_day.DATA}}**天
+
+{{birthday_message.DATA}}
+
+--- 
+
+{{note_en.DATA}}
+{{note_ch.DATA}}
     `
   },
 ]
