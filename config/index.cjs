d4f418f0807b7265c4bf10e649513fbc38b2550a
Fenta <44316203+wkx160312746@users.noreply.github.com>
2023-04-04 17:56:07 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -61,7 +61,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝早上好~记得带🔑!',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '4baa2f2fa8194d6892e100e9fc2d5f71',   // c5eca7dfa93b4754bbe50a411380cc35
+      id: 'c5eca7dfa93b4754bbe50a411380cc35',   // c5eca7dfa93b4754bbe50a411380cc35
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
