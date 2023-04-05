336617f700927f681839b0737c54d591073fba65
Fenta <44316203+wkx160312746@users.noreply.github.com>
2023-04-05 23:48:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -63,7 +63,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'c5eca7dfa93b4754bbe50a411380cc35',   // c5eca7dfa93b4754bbe50a411380cc35
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	0001', // 0001
+      useTemplateId: '0001', // 0001
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
